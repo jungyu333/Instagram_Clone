8966e719487bd43facac9f3b5ff2a2ca598e5f62
jungyu <jungyu@naver.com>
2022-07-29 23:16:53 +0900

    스토리 리팩토링

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,8 +1,24 @@
 import React from "react";
+import styled from "styled-components";
 import Layout from "../components/Layout";
+import Story from "../components/Story";
+
+const Wrapper = styled.div`
+  background-color: #fafafa;
+  width: 100%;
+  height: 100vh;
+  padding: 20px 0;
+  margin: auto 0;
+`;
 
 function Home() {
-  return <Layout>Home</Layout>;
+  return (
+    <Layout>
+      <Wrapper>
+        <Story />
+      </Wrapper>
+    </Layout>
+  );
 }
 
 export default Home;
