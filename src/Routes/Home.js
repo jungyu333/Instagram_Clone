a3c8e87903e4a14ff8fd21fa6c2cf88a035df9db
eogns47 <eogns47@naver.com>
2022-08-01 19:57:06 +0900

    sidemenu 구현

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import Card from "../components/Card";
 import Layout from "../components/Layout";
+import SideMenu from "../components/SideMenu";
 import Story from "../components/Story";
 
 const Wrapper = styled.div`
@@ -12,15 +13,18 @@ const Wrapper = styled.div`
   margin: 0 auto;
 `;
 
+
 function Home() {
   return (
     <Layout>
-      <Wrapper>
-        <Story />
-        {[1, 2, 3, 4, 5, 6].map((card) => (
-          <Card key={card} />
-        ))}
-      </Wrapper>
+        <Wrapper>
+          <Story />
+          <SideMenu />
+          {[1, 2, 3, 4, 5, 6].map((card) => (
+            <Card key={card} />
+          ))}
+          
+        </Wrapper>
     </Layout>
   );
 }
