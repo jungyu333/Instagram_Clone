308efa6ae7345f35b54eedc24ed614aa6aede97f
jungyu333 <96876293+jungyu333@users.noreply.github.com>
2022-08-04 23:04:26 +0900

    Revert "wootae first commit"

--- src/Router.js
+++ src/Router.js
@@ -2,8 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Direct from "./Routes/Direct";
 import Home from "./Routes/Home";
 import Profile from "./Routes/Profile";
-import ProfileChange from "./Routes/ProfileChange";
-import PasswordChange from "./Routes/PasswordChange"
 
 function Router() {
   return (
@@ -12,8 +10,6 @@ function Router() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/direct" element={<Direct />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
-        <Route path="/profileChange" element={<ProfileChange />}></Route>
-        <Route path="PasswordChange" element={<PasswordChange />}></Route>
       </Routes>
     </BrowserRouter>
   );
